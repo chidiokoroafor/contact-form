6699af97ae254a280d69f2d1f5aa9069ca7c883a
chidiokoroafor <cokoroafor@healthstation.ng>
2024-12-03 03:41:07 +0100

    update design

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <main className="w-full min-h-screen font-Karla bg-Green-200  text-Grey-900 flex justify-center items-center">
       <Toaster richColors position="top-center" />
-      <section className="w-[90%] max-w-[670px] bg-White rounded-xl p-6">
+      <section className="w-[90%] max-w-[670px] my-8 bg-White rounded-xl p-6">
         <h2 className="font-bold text-2xl mb-4">Contact Us</h2>
         <ContactForm />
     </section>
